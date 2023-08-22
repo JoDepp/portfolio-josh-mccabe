99cc9983ce6a16db32cb882514dd7a1ae255cd83
jody_d <jodydepp@gmail.com>
2023-08-22 09:32:11 -0400

    CSS updates, simplified website

--- script/script.js
+++ script/script.js
@@ -17,3 +17,27 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+
+//email validation
+
+      function validateEmail(email) {
+        let res = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        return res.test(email);
+      }
+      function validate() {
+        let result = $("#result");
+        let email = $("#email").val();
+        result.text("");
+        if(validateEmail(email)) {
+          result.text(email + " is valid");
+          result.css("color", "blue");
+        } else {
+          result.text(email + " is not valid");
+          result.css("color", "red");
+        }
+        return false;
+      }
+      $("#validate").on("click", validate);
+ 
+
+   
